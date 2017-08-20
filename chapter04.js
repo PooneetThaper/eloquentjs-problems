b435c9bed643874f374b1c9cf77198e5b860113a
PooneetThaper <pooneetthaper@gmail.com>
2017-08-19 23:11:11 -0400

    Moved solutions over to submission format

--- chapter04.js
+++ chapter04.js
@@ -9,37 +9,83 @@
 
 // Problem 1: The sum of a range
 function range(start, end, step=1) {
-  // Your code here
+  var retval = [];
+  for(var i=min; i<=max; i+=step){
+    retval[retval.length]=i;
+  }
+  return retval;
 }
 
 function sum(array) {
-  // Your code here
+  var retval = 0;
+  for(var i=0; i<array.length; i++){
+    retval += array[i];
+  }
+  return retval;
 }
 
 // Problem 2: Reversing an Array
 function reverseArray(array) {
-  // Your code here
+  var retval = [];
+  for(var i=array.length; i>0; i--){
+    retval[retval.length] = array[i-1];
+  }
+  return retval;
+}
+
+function swap(array, indexA, indexB){
+  var i = array[indexA];
+  array[indexA] = array[indexB];
+  array[indexB] = i;
 }
 
 function reverseArrayInPlace(array) {
-  // Your code here
+  for(var i=0; i<(array.length/2); i++){
+    swap(array, i, array.length-i-1)
+  }
 }
 
 // Problem 3: A List
 function arrayToList(array) {
-  // Your code here
+  var list = null;
+  for(var i=array.length-1; i>=0; i--){
+    list = prepend(array[i], list);
+  }
+  return list;
 }
 
 function listToArray(list) {
-  // Your code here
+  var retval = [];
+  while(list != null){
+    retval[retval.length] = list.value;
+    list = list.list;
+  }
+  return retval;
+}
+
+function listToArrayRecursive(list){
+  var retval = [list.value];
+  if (list.list != null){
+    var returned = listToArrayRecursive(list.list);
+    for(var i=0; i<returned.length; i++){
+      retval[retval.length] = returned[i];
+    }
+  }
+  return retval;
 }
 
 function nth(list, position) {
-  // Your code here
+  if (position===0){
+    return list.value;
+  }else if(list.list != null){
+    return nth(list.list, position-1);
+  }else{
+    return undefined;
+  }
 }
 
 function prepend(element, list) {
-  // Your code here
+  return {value:element, list:list}
 }
 
 // Problem 4: Deep comparison
